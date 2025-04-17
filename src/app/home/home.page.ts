86e099c550fee215af5f3e762ddae94ba53d0541
foonie <funiesadiki@gmail.com>
2025-04-17 11:53:43 +0200

    Display Results

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { WeatherService } from '../weather.service';
+import { IonHeader } from "@ionic/angular/standalone";
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  standalone: false,
+  standalone: true, 
+  imports: [IonicModule, FormsModule, CommonModule],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss']
 })
 export class HomePage {
+  city = '';
+  weatherData: any;
+  favorites: string[] = [];
+
+  constructor(private weatherService: WeatherService) {
+    this.loadFavorites();
+  }
+
+  getWeather() {
+    this.weatherService.getWeather(this.city).subscribe(data => {
+      this.weatherData = data;
+    }, err => {
+      alert('City not found');
+    });
+  }
 
   constructor() {}
 
