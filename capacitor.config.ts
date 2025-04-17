75f3029617000af6a29630b7230cf02f926abe2f
foonie <funiesadiki@gmail.com>
2025-04-17 10:43:59 +0200

    Set up files
    ionic angular initial set up files

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'weather_app_angular',
+  webDir: 'www'
+};
+
+export default config;
